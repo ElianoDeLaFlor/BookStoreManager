4388c1d1fdbd248e7fa56a8e4c149fc1faf294a9
ElianoDeLaFlor <48012583+ElianoDeLaFlor@users.noreply.github.com>
2025-01-19 11:38:28 +0400

    Add data context

---
+++ BookStoreManager.Persistence/DataContext/DataContext.cs
@@ -0,0 +1,8 @@
+using BookStoreManager.Persistence.RepositoriesInterfaces;
+
+namespace BookStoreManager.Persistence.DataContext;
+
+public class DataContext:IDataContext
+{
+    
+}
---
+++ BookStoreManager.Persistence/Interfaces/IDataContext.cs
@@ -0,0 +1,5 @@
+namespace BookStoreManager.Persistence.RepositoriesInterfaces;
+
+public class IDataContext
+{
+}
